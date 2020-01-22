47bf2b57a8bb26003dfa586c4ea1608493414aa6
Ana Gainaru <ana.gainaru@gmail.com>
2020-01-22 12:17:13 -0500

    Examples of errors and faults

---
+++ C_code_examples/faulty/sigfault.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[])                 
+{
+      int a[10];
+      int i = -872625577;
+
+      a[i] = 1234;
+      printf("i = %d\n", i);
+      printf("a[i] = %d\n", a[i]);
+}
